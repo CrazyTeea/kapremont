ea84494460296bd2ad192a3295592548b1659131
Crazyteea <lipatow.nikita@yandex.ru>
2020-03-15 23:04:48 +0300

    fix

--- frontend/src/components/pages/program/form/Svedenia.vue
+++ frontend/src/components/pages/program/form/Svedenia.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="hidden-val-for-table">
         <b-table
-            bordered
-            foot-clone
-            hover
-            caption-top
-            :fields="fields"
-            :items="stages"
-            >
-                <template v-slot:cell(is_nessesary)="row">
-                    <input type="hidden" v-model="sved.is_nessesary[row.index]" :name="`${modelName}[${row.index}][is_nessesary]`">
-                    <v-select
+                bordered
+                foot-clone
+                hover
+                caption-top
+                :fields="fields"
+                :items="stages"
+        >
+            <template v-slot:cell(index)="row">
+                {{row.index+1}}
+            </template>
+            <template v-slot:cell(is_nessesary)="row">
+                <input type="hidden" v-model="sved.is_nessesary[row.index]" :name="`${modelName}[${row.index}][is_nessesary]`">
+                <v-select
                         class="mt-5"
                         v-model="sved.is_nessesary[row.index]"
                         label="type"
@@ -19,59 +22,59 @@
                             {id:0,type:'Нет'}
                         ]"
                         :reduce = "type => type.id"
-                    />
-                </template>
-                <template v-slot:cell(begin_date)="row">
-                    <b-form-input type="date" v-model="sved.begin_date[row.index]" :name="`${modelName}[${row.index}][date_event_start]`" />
-                    <!--<b-form-datepicker
-                        today-button
-                        label-today-button="сегодня"
-                        reset-button
-                        label-reset-button="сбросить"
-                        placeholder="дата"
-                        size="sm"
-                        :name="`${modelName}[${row.index}][date_event_start]`"
-                        v-model="sved.begin_date[row.index]">
-                    </b-form-datepicker>-->
-                </template>
-                <template v-slot:cell(final_date)="row">
-                    <b-form-input type="date" v-model="sved.final_date[row.index]" :name="`${modelName}[${row.index}][date_event_end]`" />
-                    <!--<b-form-datepicker
-                        today-button
-                        label-today-button="сегодня"
-                        reset-button
-                        label-reset-button="сбросить"
-                        size="sm"
-                        placeholder="дата"
-                        :name="`${modelName}[${row.index}][date_event_end]`"
-                        v-model="sved.final_date[row.index]"></b-form-datepicker>-->
-                </template>
-                <template v-slot:cell(rc)="row">
-                    <b-form-input
-                        v-if="row.value" 
-                        v-model="sved.realization_cost[row.index]" 
+                />
+            </template>
+            <template v-slot:cell(begin_date)="row">
+                <b-form-input type="date" v-model="sved.begin_date[row.index]" :name="`${modelName}[${row.index}][date_event_start]`" />
+                <!--<b-form-datepicker
+                    today-button
+                    label-today-button="сегодня"
+                    reset-button
+                    label-reset-button="сбросить"
+                    placeholder="дата"
+                    size="sm"
+                    :name="`${modelName}[${row.index}][date_event_start]`"
+                    v-model="sved.begin_date[row.index]">
+                </b-form-datepicker>-->
+            </template>
+            <template v-slot:cell(final_date)="row">
+                <b-form-input type="date" v-model="sved.final_date[row.index]" :name="`${modelName}[${row.index}][date_event_end]`" />
+                <!--<b-form-datepicker
+                    today-button
+                    label-today-button="сегодня"
+                    reset-button
+                    label-reset-button="сбросить"
+                    size="sm"
+                    placeholder="дата"
+                    :name="`${modelName}[${row.index}][date_event_end]`"
+                    v-model="sved.final_date[row.index]"></b-form-datepicker>-->
+            </template>
+            <template v-slot:cell(rc)="row">
+                <b-form-input
+                        v-if="row.value"
+                        v-model="sved.realization_cost[row.index]"
                         placeholder="Цена"
                         step="0.001"
                         type="number"
                         @change="val => setFloat(val,row.index,'realization_cost')"
                         :name="`${modelName}[${row.index}][cost_real]`"
-                        ></b-form-input>
-                        <label v-else>-</label>
-                </template>
-                <template v-slot:cell(kap_cost)="row">
-                    <b-form-input
-                        v-if="row.value" 
-                        v-model="sved.kap_cost[row.index]" 
+                ></b-form-input>
+                <label v-else>-</label>
+            </template>
+            <template v-slot:cell(kap_cost)="row">
+                <b-form-input
+                        v-if="row.value"
+                        v-model="sved.kap_cost[row.index]"
                         placeholder="Цена"
                         step="0.001"
                         type="number"
                         @change="val => setFloat(val,row.index,'kap_cost')"
                         :name="`${modelName}[${row.index}][sum_bud_fin]`"
-                        ></b-form-input>
-                        <label v-else>-</label>
-                </template>
-                <template v-slot:cell(finanse)="row">
-                    <b-form-input
+                ></b-form-input>
+                <label v-else>-</label>
+            </template>
+            <template v-slot:cell(finanse)="row">
+                <b-form-input
                         v-if="row.value"
                         v-model="sved.finanse[row.index]"
                         placeholder="Цена"
@@ -79,36 +82,36 @@
                         type="number"
                         @change="val => setFloat(val,row.index,'finanse')"
                         :name="`${modelName}[${row.index}][fin_vnebud_ist]`"
-                        ></b-form-input>
-                        <label v-else>-</label>
-                </template>
-                <template v-slot:foot(stage)>
-                    <span class="font-weight-bold">ИТОГО:</span>
-                </template>
-                <template v-slot:foot(is_nessesary)>
-                    <span class="font-weight-bold"></span>
-                </template>
-                <template v-slot:foot(begin_date)>
-                    <span class="font-weight-bold"></span>
-                </template>
-                <template v-slot:foot(final_date)>
-                    <span class="font-weight-bold"></span>
-                </template>
-                <template v-slot:foot(rc)>
-                    <span class="font-weight-bold">{{ rc_full }}</span>
-                </template>
-                <template v-slot:foot(kap_cost)>
-                    <span class="font-weight-bold">{{ kap_full }}</span>
-                </template>
-                <template v-slot:foot(finanse)>
-                    <span class="font-weight-bold">{{ finanse_sum }}</span>
-                </template>
+                ></b-form-input>
+                <label v-else>-</label>
+            </template>
+            <template v-slot:foot(stage)>
+                <span class="font-weight-bold">ИТОГО:</span>
+            </template>
+            <template v-slot:foot(is_nessesary)>
+                <span class="font-weight-bold"></span>
+            </template>
+            <template v-slot:foot(begin_date)>
+                <span class="font-weight-bold"></span>
+            </template>
+            <template v-slot:foot(final_date)>
+                <span class="font-weight-bold"></span>
+            </template>
+            <template v-slot:foot(rc)>
+                <span class="font-weight-bold">{{ rc_full }}</span>
+            </template>
+            <template v-slot:foot(kap_cost)>
+                <span class="font-weight-bold">{{ kap_full }}</span>
+            </template>
+            <template v-slot:foot(finanse)>
+                <span class="font-weight-bold">{{ finanse_sum }}</span>
+            </template>
         </b-table>
     </div>
 </template>
 
 <script>
-import Multiselect from 'vue-select';
+    import Multiselect from 'vue-select';
 
     export default {
         name: "Svedenia",
@@ -125,7 +128,7 @@ import Multiselect from 'vue-select';
             }
         },
         components: {
-            "v-select": Multiselect,    
+            "v-select": Multiselect,
         },
         methods:{
             setFloat(val,index, attr) {
@@ -138,7 +141,7 @@ import Multiselect from 'vue-select';
             }
         },
         props:{
-          modelName:String
+            modelName:String
         },
         computed: {
             test(){
@@ -170,6 +173,7 @@ import Multiselect from 'vue-select';
         data() {
             return {
                 fields: [
+                    {key: 'index', 'label':'№'},
                     {key: 'stage', label: 'Этап'},
                     {key: 'is_nessesary', label: 'Необходимость выполнения'},
                     {key: 'begin_date', label: 'Дата начала', tdClass: 'date vertical-align-extra-table'},
@@ -180,13 +184,13 @@ import Multiselect from 'vue-select';
                 ],
                 stages: [
                     {
-                        'stage': 'Проведение тендера и заключение договора на выполнение обследования', 
+                        'stage': 'Проведение тендера и заключение договора на выполнение обследования',
                         'rc': false,
                         'kap_cost': false,
                         'finanse':  false
                     },
                     {
-                        'stage': 'Выполнение обследования, подготовка и утверждение дефектного акта', 
+                        'stage': 'Выполнение обследования, подготовка и утверждение дефектного акта',
                         'rc': true,
                         'kap_cost': false,
                         'finanse':  true
@@ -235,7 +239,7 @@ import Multiselect from 'vue-select';
                     realization_cost: [],
                     kap_cost: [],
                     finanse:[]
-                }, 
+                },
                 masObj: [
                     {
                         is_nessesary: true,
@@ -258,11 +262,11 @@ import Multiselect from 'vue-select';
 </style>
 
 <style>
-.hidden-val-for-table {
-    overflow: hidden !important;
-    overflow-x: scroll !important;
-}
-.vertical-align-extra-table {
-    vertical-align: middle !important;
-}
+    .hidden-val-for-table {
+        overflow: hidden !important;
+        overflow-x: scroll !important;
+    }
+    .vertical-align-extra-table {
+        vertical-align: middle !important;
+    }
 </style>
