00c38b8b5631c75654176d8e48146b39255d6a29
CrazyTea <lipatow.nikita@yandex.ru>
2020-03-20 17:14:56 +0300

    sdfsdf

--- frontend/src/components/pages/organization/info/ListObjectComponent.vue
+++ frontend/src/components/pages/organization/info/ListObjectComponent.vue
@@ -1,79 +1,125 @@
 <template>
-    <b-card no-body>
-        <b-card-header header-tag="header" class="p-1" role="tab">
-            <span> <b-icon-gear-wide-connected />Объекты требующие капитального ремонта </span>
-        </b-card-header>
-        <b-card-body class="hidden-overflow">
-            <b-table-simple bordered hover>
-                <b-thead>
-                    <b-tr>
-                        <b-th>№</b-th>
-                        <b-th>Приоритет</b-th>
-                        <b-th>Субъект РФ</b-th>
-                        <b-th>Наименование объекта, требующего кап. ремонта</b-th>
-                        <b-th>Назначение</b-th>
-                        <b-th>Площадь кап. ремонта (кв.м)</b-th>
-                        <b-th>Адрес объекта</b-th>
-                        <b-th>Год постройки</b-th>
-                        <b-th>Износ здания</b-th>
-                        <b-th>Предписания надзорных органов: МЧС, Роспотребнадзор и т.д. (при наличии)</b-th>
-                        <b-th>Вид планируемого мероприятия</b-th>
-                        <b-th>Сумма бюджетного финансирования на проведение кап.ремонта (тыс. руб)</b-th>
-                        <b-th>Софинансирование из внебюджетных источников (тыс. руб)</b-th>
-                        <b-th>Примечание</b-th>
-                    </b-tr>
-                </b-thead>
-                <b-tbody>
-                    <b-tr v-for="(item, index) in items" :key="index" @click="goToObj(item.id)">
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ index + 1 }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.id_priority }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell cursor-pointer center-text-in-cell">
-                            <label class="cursor-pointer">{{ item.city }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.name }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.assignment }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.square_kap }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.address }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.year }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ getIznos(item.wear) }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.regulation }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer"></label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.finance_sum }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">{{ item.coFinancing }}</label>
-                        </b-th>
-                        <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
-                            <label class="cursor-pointer">нет</label>
-                        </b-th>
-                    </b-tr>
-                </b-tbody>
-            </b-table-simple>
+    <div>
+        <b-card no-body>
+            <b-card-header header-tag="header" class="p-1" role="tab">
+                <span> <b-icon icon="bookmark-fill" scale="1.5" class="mr-2 ml-1"></b-icon>Подробная информация</span>
+            </b-card-header>
+            <b-card-body>
+                <b-table-simple bordered hover>
+                    <b-thead>
+                        <b-tr>
+                            <b-th
+                                >Объем финансового обеспечения на реализацию мероприятий по модернизации инфраструктуры,
+                                включая капитальный ремонт объектов и проведение мероприятий по антитеррористической
+                                защищенности объектов,тыс. рублей</b-th
+                            >
+                            <b-th
+                                >Из них на реализацию мероприятий по антитеррористической защищенности, не более,тыс.
+                                рублей</b-th
+                            >
+                            <b-th>Предельная стоимость капитального ремонта 1 кв. м. площади, рублей</b-th>
+                            <b-th>Ожидаемая площадь капитального ремонта, кв.м.</b-th>
+                        </b-tr>
+                    </b-thead>
+                    <b-tbody>
+                        <b-tr>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ programm.finance_volume }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ programm.finance_events }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell cursor-pointer center-text-in-cell">
+                                <label class="cursor-pointer">{{ programm.cost }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ waited_square }}</label>
+                            </b-th>
+                        </b-tr>
+                    </b-tbody>
+                </b-table-simple>
+            </b-card-body>
+        </b-card>
 
-            <b-pagination v-show="currentPage" :per-page="perPage" v-model="currentPage" />
-        </b-card-body>
-    </b-card>
+        <b-card no-body class="mt-4">
+            <b-card-header header-tag="header" class="p-1" role="tab">
+                <span>
+                    <b-icon icon="gear-wide-connected" scale="1.5" class="mr-2 ml-1"></b-icon>Объекты требующие
+                    капитального ремонта
+                </span>
+            </b-card-header>
+            <b-card-body class="hidden-overflow">
+                <b-table-simple bordered hover>
+                    <b-thead>
+                        <b-tr>
+                            <b-th>№</b-th>
+                            <b-th>Приоритет</b-th>
+                            <b-th>Субъект РФ</b-th>
+                            <b-th>Наименование объекта, требующего кап. ремонта</b-th>
+                            <b-th>Назначение</b-th>
+                            <b-th>Площадь кап. ремонта (кв.м)</b-th>
+                            <b-th>Адрес объекта</b-th>
+                            <b-th>Год постройки</b-th>
+                            <b-th>Износ здания</b-th>
+                            <b-th>Предписания надзорных органов: МЧС, Роспотребнадзор и т.д. (при наличии)</b-th>
+                            <b-th>Вид планируемого мероприятия</b-th>
+                            <b-th>Сумма бюджетного финансирования на проведение кап.ремонта (тыс. руб)</b-th>
+                            <b-th>Софинансирование из внебюджетных источников (тыс. руб)</b-th>
+                            <b-th>Примечание</b-th>
+                        </b-tr>
+                    </b-thead>
+                    <b-tbody>
+                        <b-tr v-for="(item, index) in items" :key="index" @click="goToObj(item.id)">
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ index + 1 }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.id_priority }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell cursor-pointer center-text-in-cell">
+                                <label class="cursor-pointer">{{ item.city }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.name }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.assignment }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.square_kap }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.address }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.year }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ getIznos(item.wear) }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.regulation }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer"></label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.finance_sum }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">{{ item.coFinancing }}</label>
+                            </b-th>
+                            <b-th class="normal-font-weight-for-sell center-text-in-cell cursor-pointer">
+                                <label class="cursor-pointer">нет</label>
+                            </b-th>
+                        </b-tr>
+                    </b-tbody>
+                </b-table-simple>
+
+                <b-pagination v-show="currentPage" :per-page="perPage" v-model="currentPage" />
+            </b-card-body>
+        </b-card>
+    </div>
 </template>
 
 <script>
@@ -81,6 +127,7 @@ import Axios from "axios";
 export default {
     data() {
         return {
+            programm: {},
             currentPage: 1,
             perPage: 5,
             csrf: document.getElementsByName("csrf-token")[0].content,
@@ -88,18 +135,28 @@ export default {
             items: []
         };
     },
-    mounted() {
+    async mounted() {
         this.org_id = this.$route.params.id;
-        this.getCurentOrg();
+        await this.getCurentOrg();
+        console.log(this.programm);
+    },
+    computed: {
+        waited_square() {
+            return (
+                ((parseFloat(this.programm.finance_volume) - parseFloat(this.programm.finance_events)) * 1000) /
+                parseFloat(this.programm.cost)
+            ).toFixed(2);
+        }
     },
     methods: {
-        getCurentOrg() {
-            Axios.post(`/api/org-table/${this.org_id}`, null, {
+        async getCurentOrg() {
+            return Axios.post(`/api/org-table/${this.org_id}`, null, {
                 headers: {
                     "X-CSRF-Token": this.csrf
                 }
             }).then(res => {
-                this.items = res.data;
+                this.items = res.data.objects;
+                this.programm = res.data.programm;
             });
         },
         getIznos(iznos) {
