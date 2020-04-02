6a4185e16585f5ef9a1d18592e0f7130d0c083ba
Crazyteea <lipatow.nikita@yandex.ru>
2020-04-02 19:36:06 +0300

    admin

--- widgets/yii2-admin/controllers/UserController.php
+++ widgets/yii2-admin/controllers/UserController.php
@@ -159,7 +159,7 @@ class UserController extends Controller
                 return $this->goHome();
             }
         }
-        $orgs = ArrayHelper::map(Organizations::find()->joinWith(['program'])->where(['system_status'=>1,'program.system_status'=>1])->all(),'id','name');
+        $orgs = ArrayHelper::map(Organizations::find()->joinWith(['program'])->where([Organizations::tableName().'.system_status'=>1,'program.system_status'=>1])->all(),'id','name');
 
         return $this->render('signup',compact('model','orgs'));
     }
