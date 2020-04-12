77cbb2352337f3c02ec9598c1045f7976c893ecf
Crazyteea <lipatow.nikita@yandex.ru>
2020-04-13 01:05:28 +0300

    pzdc dohuya govna

---
+++ controllers/api/ShitController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\controllers\api;
+
+
+class ShitController
+{
+
+}
---
+++ frontend/src/components/organisms/OrganizationShitTable/OrgShitView.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "OrgShitView"
+    }
+</script>
+
+<style scoped>
+
+</style>
