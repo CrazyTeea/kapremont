e417a50b3d830bdd4a0e1506a904b6a5fc8b6ac6
Crazyteea <lipatow.nikita@yandex.ru>
2020-05-13 18:01:51 +0300

    hotelki pidarov

--- frontend/src/components/pages/checkMgsu/MainCheckComponent.vue
+++ frontend/src/components/pages/checkMgsu/MainCheckComponent.vue
@@ -38,7 +38,7 @@
             </b-tab>
 
             <b-tab lazy v-can:root,mgsu,dep title-link-class="text-success" title="Рассмотрено ДЭП">
-                <v-objects status="2" dep_status="approved" />
+                <v-objects dep_status="approved" />
             </b-tab>
 
             <b-tab lazy v-can:root,mgsu,dep title-link-class="text-warning" title="Резерв">
