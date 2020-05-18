4946df57339704a5e4491aee185453ea5b06f7cb
Crazyteea <lipatow.nikita@yandex.ru>
2020-05-18 10:07:57 +0300

    f

--- frontend/src/components/pages/organization/info/ObjectShowComponent.vue
+++ frontend/src/components/pages/organization/info/ObjectShowComponent.vue
@@ -1505,22 +1505,15 @@ export default {
 
             });
          //  console.log( this.svedenia2);
-           for (let i =0; i <= this.svedenia2.length; i++) {
-               let item = this.svedenia2[i];
-               console.log(item);
-               if (item.hasOwnProperty('done') && !item.done){
-                   if (item.date_event_end)
-                       this.dateStatus = new Date(item.date_event_end).toLocaleDateString();
-                   return true;
-               }
-           }
-           /* this.svedenia2.some(item=>{
+
+            this.svedenia2.some(item=>{
                 if (item.done == 0){
-                    if (item.date_event_end)
+                    if (item.date_event_end) {
                         this.dateStatus = new Date(item.date_event_end).toLocaleDateString();
-                    return true;
+                        return true;
+                    }
                 }
-            });*/
+            });
         },
         getIznos(iznos) {
             let izn = parseInt(iznos);
