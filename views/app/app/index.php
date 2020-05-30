db6ee884318249e7dbb4bc7e9c62562704e27841
Crazyteea <lipatow.nikita@yandex.ru>
2020-05-30 14:03:11 +0300

    clean code

--- views/app/app/index.php
+++ views/app/app/index.php
@@ -1,12 +1,6 @@
-<div>
-говно жопа
-</div>
+
 <?php
 
-use app\models\ProgramObjects;
-
-// $model = ProgramObjects::find()-> where(['id' => 115, 'system_status' => 1])->all();
-// var_dump($model);
 
 ?>
 <?php foreach($model as $program): ?>
